d6e078e12e5fbf1ffe2015320ab3794b6d3432d0
bennylupattelli <benedetta.gencarelli.24@ucl.ac.uk>
2025-07-16 15:11:38 +0100

    Add files via upload
    manually uploading utility scripts

--- Assets/Scripts/Utility/GeneralGlobals.cs
+++ Assets/Scripts/Utility/GeneralGlobals.cs
@@ -1,6 +1,8 @@
 
 using System.Collections.Generic;
+using Mono.CSharp;
 using Unity.Collections;
+using UnityEditor;
 using UnityEngine;
 
 namespace Globals
@@ -16,30 +18,69 @@ namespace Globals
         public static Dictionary<int, int> repeatsDict = new Dictionary<int,int>{{highScore,3},
                                                                                  {lowScore,1}};  // Reward sound repeats
                                                                                                  // for each increment val
+                                                                                                 // public static float probabilityRisky = 0.4f;
 
         public static string highScoreRewardType = "High";
         public static string lowScoreRewardType = "Low";
-        public static List<string> trialTypes = new List<string>{"HighLow", "ForcedHigh", "ForcedLow"};
-        public static List<int> trialTypeProbabilities = new List<int>{80, 10, 10};
-        public static List<int> wallSeparations = new List<int>{1,2,4};   // index difference between trial walls
+        public static string zeroRewardType = "Zero";
+        // og risky choice
+        // public static List<string> trialTypes = new List<string>{"HighLow", "RiskyChoice", "ForcedHigh", "ForcedLow"};
+        
+        // solo sessions
+        // public static List<string> trialTypes = new List<string>{"HighLow", "RiskyChoice", "ForcedHigh", "ForcedLow", "ForcedRisky"};
+
+        // socials
+        public static List<string> trialTypes = new List<string>{"HighLow"};
+
+        // og risky choicee
+        // public static List<int> trialTypeProbabilities = new List<int>{40, 40, 10, 10};
+        
+        // solo sessions
+        // public static List<int> trialTypeProbabilities = new List<int>{35, 35, 10, 10, 10};
+
+        // socials
+        public static List<int> trialTypeProbabilities = new List<int>{50, 50};
+
+        // og risky choice
+        // public static List<int> wallSeparations = new List<int>{1,2,4};   // index difference between trial walls
                                                                           // random choice within list
 
-        public static List<int> wallSeparationsProbabilities = new List<int>{50,25,25};
-        public static Color wallHighColour = Color.blue; // Color.red
-        public static Color wallLowColour = Color.red;  // Color.blue
+        // solo sessions
+        // public static List<int> wallSeparations = new List<int>{1,2}; 
+
+        // socials
+        public static List<int> wallSeparations = new List<int>{1,2};
+
+        // og risky choice
+        // public static List<int> wallSeparationsProbabilities = new List<int>{50,25,25};
+
+
+        // solo sessions
+        // public static List<int> wallSeparationsProbabilities = new List<int>{50,50};
+
+        // socials
+        public static List<int> wallSeparationsProbabilities = new List<int>{50,50};
+
+        public static Color wallHighColour = Color.magenta;
+        public static Color wallLowColour = Color.cyan;
+        // public static Color wallRiskyColour = Color.yellow;
+        public static Material wallHighMaterial; // Material for high wall
+        public static Material wallLowMaterial;  // Material for low wall
+        // public static Material wallRiskyMaterial; // Material for risky wall
         public static Color wallInteractionZoneColour = new Color(1, 215/255f, 0, 129/255f);
         public static bool automaticStartTrial = false;
 
-        public static float globalIlluminationLow = 0.6f;
-        public static float globalIlluminationHigh = 0.8f;
-
-        // timings
-        public static float ITIMin = 2f;
-        public static float ITIMax = 5f;
+        // timings - reduced for agents
+        // public static float ITIMin = 2f;
+        public static float ITIMin = 1f;
+        //public static float ITIMax = 5f;
+        public static float ITIMax = 2f;
         public static float trialEndDuration = 2f;
         public static float trialStartDurationMin = 0.5f;
         public static float trialStartDurationMax = 1.5f; 
         public static float startFirstTrialDelay = 0.75f;
+        public static float globalIlluminationLow = 0.6f;
+        public static float globalIlluminationHigh = 0.8f;
 
 
         // movement
@@ -61,8 +102,11 @@ namespace Globals
 
         // Fixedstring values
         public static FixedString32Bytes highLow = new("HighLow");
+        // public static FixedString32Bytes riskyChoice = new("RiskyChoice");
+
         public static FixedString32Bytes forcedHigh = new("ForcedHigh");
         public static FixedString32Bytes forcedLow = new("ForcedLow");
-
+        // public static FixedString32Bytes forcedRisky = new("ForcedRisky");
+        
     }
 }
