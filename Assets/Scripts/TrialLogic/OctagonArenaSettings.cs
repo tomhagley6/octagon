ad6563d38cb26bdf7207cb0f95ce0bb39a4fb48d
tomhagley6 <tomhagley@btinternet.com>
2026-03-10 11:39:52 +0000

    Added a 1.0-1.5 second variable TrialStart delay, and 2 second TrialEnd delay that sandwiches the ITI (s.t. the delays in the trial logic are now the same as in the experimental setup. Effectively, this roughly doubles the ITI and adds in a small amount of variability. No change in global illumination has been included here, as the agents are not responsive to illumination.

--- Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
+++ Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
@@ -319,9 +319,16 @@ public class OctagonArenaSettings : MonoBehaviour
         Debug.Log($"ITI range: {General.ITIMin} to {General.ITIMax}");
         #endif
         iti = Random.Range(General.ITIMin, General.ITIMax);
+        
+        // Use a 2 second fixed EndTrial delay to replicate experimental setup, added 260310
+        yield return new WaitForSeconds(2f);
 
         //Debug.Log($"Waiting for ITI: {iti}");
         yield return new WaitForSeconds(iti);
+
+        // Use a 0.5-1.5 second variable length TrialStart delay to replicate experimental setup, added 260310
+        float trialStartDelay = Random.Range(0.5f, 1.5f);
+        yield return new WaitForSeconds(trialStartDelay);
 
         #if UNITY_EDITOR
         Debug.Log("Trial loop started.");
