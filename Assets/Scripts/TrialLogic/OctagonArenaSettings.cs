6b693fa9a38b9ad91c2d584153b4b33f462513f6
benny <benedetta.gencarelli.24@ucl.ac.uk>
2026-03-18 16:06:08 +0000

    Additions to OctagonArenaSettings for Logging purposes. Added variables to keep track of agents' scres, trial number, trial active status, and slice onset event. Slice onset is invoked in ColourWalls. Trial is set to active after the iti in the ITI IEnumerator and trial scores are reset. TrialActive is set back to false in ResetTrial method. New methods at the bottom are responsible for the following: 1) Invoke that TrialActive status has changed when SetTrialActive is called. 2) Reset agents' scores back to zero at the start of each trial. 3) Set trial scores to the trial-relevant values. This will be called when an active wall is triggered and new reward outcomes are assigned. 4) Increment the trial number and invoke event that trial number has changed for logging.

--- Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
+++ Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +6,8 @@ using Globals;
 using KaimiraGames;
 using Unity.MLAgents;
 using UnityEngine;
-using UnityEditor;
 // using System.Numerics;
+using Random = UnityEngine.Random;
 
 public class OctagonArenaSettings : MonoBehaviour
 {
@@ -44,6 +45,17 @@ public class OctagonArenaSettings : MonoBehaviour
     // Training curriculum parameters
     private EnvironmentParameters envParams;
     private float arenaScale;
+
+    // Logging events and variables
+    // scores
+    public int PlayerTrialScore { get; private set; } = 0;
+    public int OpponentTrialScore { get; private set; } = 0;
+    // trial events and trial number
+    public event Action SliceOnset;
+    public bool TrialActive { get; private set; } //  non-networked trial active boolean
+    public event Action<bool,bool> TrialActiveChanged; // added to equate to OnValueChanged in Netcode
+    public ushort trialNum { get; private set; } = 0;
+    public event Action<ushort, ushort> TrialNumChanged;
 
 
     // References for the arena and identity manager of the arena walls
@@ -293,7 +305,9 @@ public class OctagonArenaSettings : MonoBehaviour
         #if UNITY_EDITOR
         Debug.Log("[ColourWalls] New trial walls are coloured.");
         #endif
-        playerAgent.LogSliceOnsetEvent(wallID1, wallID2, thisTrialType);
+        
+        // logging action event to signal slice onset and call the slice onset logging method through chain reaction
+        SliceOnset?.Invoke();
 
     }
 
@@ -329,6 +343,10 @@ public class OctagonArenaSettings : MonoBehaviour
         // Use a 0.5-1.5 second variable length TrialStart delay to replicate experimental setup, added 260310
         float trialStartDelay = Random.Range(0.5f, 1.5f);
         yield return new WaitForSeconds(trialStartDelay);
+
+        // logging variables signalling trial is active and scores to be reset
+        SetTrialActive(true);
+        ResetTrialScores();
 
         #if UNITY_EDITOR
         Debug.Log("Trial loop started.");
@@ -426,6 +444,8 @@ public class OctagonArenaSettings : MonoBehaviour
             Debug.Log("First episode. Walls yet to be assigned, nothing to reset.");
             #endif
         }
+
+        SetTrialActive(false);
     }
 
     // Why are we changing interaction zone colour? Could this be removed
@@ -507,5 +527,29 @@ public class OctagonArenaSettings : MonoBehaviour
 
         return (reward, rewardType);
     }
+    public void SetTrialActive(bool value)
+    {
+        if (TrialActive == value) return;
+        bool prev = TrialActive;
+        TrialActive = value;
+        TrialActiveChanged?.Invoke(prev, TrialActive);
+    }
+    public void ResetTrialScores()
+    {
+        PlayerTrialScore = 0;
+        OpponentTrialScore = 0;
+    }
 
+    public void SetTrialScores(int playerScore, int opponentScore = 0)
+    {
+        PlayerTrialScore = playerScore;
+        OpponentTrialScore = opponentScore;
+    }
+
+    public void IncrementTrialNum()
+    {
+        ushort previous = trialNum;
+        trialNum++;
+        TrialNumChanged?.Invoke(previous, trialNum);
+    }
 }
