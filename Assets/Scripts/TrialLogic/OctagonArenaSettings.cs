f774e64c1e1dc547abbfbdd0c207b8d8a86a1db2
tomhagley6 <tomhagley@btinternet.com>
2026-03-23 13:37:07 +0000

    Added 180 trials into the agents branch for the first time. The new probabilities are (50,25,25), from (50,50) equal split of 45-degree and 90-degree trials. Reverted the corrected trial timings for now to test the effect of 180 trials on agent learned strategy.

--- Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
+++ Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
@@ -320,15 +320,15 @@ public class OctagonArenaSettings : MonoBehaviour
         #endif
         iti = Random.Range(General.ITIMin, General.ITIMax);
         
-        // Use a 2 second fixed EndTrial delay to replicate experimental setup, added 260310
-        yield return new WaitForSeconds(2f);
+        // // Use a 2 second fixed EndTrial delay to replicate experimental setup, added 260310
+        // yield return new WaitForSeconds(2f);
 
         //Debug.Log($"Waiting for ITI: {iti}");
         yield return new WaitForSeconds(iti);
 
-        // Use a 0.5-1.5 second variable length TrialStart delay to replicate experimental setup, added 260310
-        float trialStartDelay = Random.Range(0.5f, 1.5f);
-        yield return new WaitForSeconds(trialStartDelay);
+        // // Use a 0.5-1.5 second variable length TrialStart delay to replicate experimental setup, added 260310
+        // float trialStartDelay = Random.Range(0.5f, 1.5f);
+        // yield return new WaitForSeconds(trialStartDelay);
 
         #if UNITY_EDITOR
         Debug.Log("Trial loop started.");
