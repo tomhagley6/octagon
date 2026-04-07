c4187e64e511efed27be0af79ebde7f5a41e8795
tomhagley6 <tomhagley@btinternet.com>
2026-04-07 13:22:56 +0100

    Added clarifying comments to the trial start and end delay code.

--- Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
+++ Assets/Scripts/TrialLogic/OctagonArenaSettings.cs
@@ -321,12 +321,14 @@ public class OctagonArenaSettings : MonoBehaviour
         iti = Random.Range(General.ITIMin, General.ITIMax);
         
         // Use a 2 second fixed EndTrial delay to replicate experimental setup, added 260310
+        // Present in builds only from 260408 onwards
         yield return new WaitForSeconds(2f);
 
         //Debug.Log($"Waiting for ITI: {iti}");
         yield return new WaitForSeconds(iti);
 
         // Use a 0.5-1.5 second variable length TrialStart delay to replicate experimental setup, added 260310
+        // Present in builds only from 260408 onwards
         float trialStartDelay = Random.Range(0.5f, 1.5f);
         yield return new WaitForSeconds(trialStartDelay);
 
